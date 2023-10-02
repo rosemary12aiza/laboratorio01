3067b85982f7cc66583641d927173d293d0f83db
¨Rosemary <rosemaryaizahuallpa@gmail.com>
2023-10-01 21:07:48 -0400

    creacion de page

---
+++ src/app/post/[id]/page.tsx
@@ -0,0 +1,8 @@
+const DetailPostPage = ({ params }: any) => {
+  return (
+    <div className='flex min-h-screen flex-col items-center justify-between p-24'>
+      Detail Post Page {params.id}
+    </div>
+  );
+};
+export default DetailPostPage;

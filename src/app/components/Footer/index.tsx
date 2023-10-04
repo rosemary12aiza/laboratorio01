4f7c63ad33245be7174704c0d823dbf45523c00b
RosemaryAiza6611 Aiza <rosemary18aiza@gmail.com>
2023-10-04 06:12:39 +0000

    componente footer

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
-
+import Link from "next/link";
+import React from "react";
+import styles from "./styles.module.css";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className={styles["footer"]}>
+      <div className={styles["container1"]}>
+        <div className={styles["container2"]}>
+          <div className={styles["container3"]}>
+            <span className={styles["text"]}>SiteMaps</span>
+            <span className={styles["text1"]}>Home</span>
+            <span className={styles["text2"]}>Post</span>
+            <span className={styles["text3"]}>About</span>
+          </div>
+          <div className={styles["container4"]}>
+            <span className={styles["text4"]}>Conectate con Nosotros</span>
+            <span className={styles["text5"]}>Facebook</span>
+            <span className={styles["text6"]}>Twiter</span>
+            <span className={styles["text7"]}>TickTock</span>
+          </div>
+        </div>
+      </div>
+      <div className={styles["container5"]}>
+        <img src="/images/next.svg" alt="image" className={styles["image"]} />
 
-export default Footer
+        <img
+          src="/images/vercel.svg"
+          alt="image1"
+          className={styles["image1"]}
+        />
+        <img
+          src="/images/seminario.jpeg"
+          alt="image2"
+          className={styles["image2"]}
+        />
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,14 @@
 import Navbar from './components/Navbar';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] 
+
+
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -21,6 +25,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         <Navbar />
+        <Footer />
         {children}
       </body>
     </html>
